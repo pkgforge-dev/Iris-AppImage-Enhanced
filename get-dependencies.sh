8c7fa4b9e4edbb47a941c055dd5cac3adcbbff21
Link4Electronics <link4electronics@gmail.com>
2026-03-09 21:40:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    clang    \
+    libdecor \
+    sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ! llvm
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package iris-emu
 
 # If the application needs to be manually built that has to be done down here
 
