a7884bc03f96387b4b8562302533ee906eb60a4e
Link4Electronics <link4electronics@gmail.com>
2026-03-09 21:40:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,29 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm cmake sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building stable version of Iris..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/allkern/iris"
+VERSION="$(curl -s https://api.github.com/repos/allkern/iris/tags | grep '"name"' | grep -v 'pre' | head -1 | cut -d '"' -f 4)"
+git clone --branch "$VERSION" --recursive --depth 1 "$REPO" ./iris
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+echo "Patching SDL version-script CMake check (upstream commit 9228b91)..."
+echo "---------------------------------------------------------------"
+sed -i 's|-Wl,--version-script=${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/dummy.sym|LINKER:SHELL:--version-script \\"${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/dummy.sym\\"|' iris/deps/SDL/cmake/macros.cmake
+sed -i 's|-Wl,--version-script=${CMAKE_CURRENT_SOURCE_DIR}/src/dynapi/SDL_dynapi.sym|LINKER:SHELL:--version-script \\"${CMAKE_CURRENT_SOURCE_DIR}/src/dynapi/SDL_dynapi.sym\\"|' iris/deps/SDL/CMakeLists.txt
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Disabling IPO/LTO (the .incbin resources cannot be found during LTO fat-linking)..."
+echo "---------------------------------------------------------------"
+sed -i 's|check_ipo_supported(RESULT LTO_SUPPORTED OUTPUT LTO_ERROR)|set(LTO_SUPPORTED FALSE)|' iris/CMakeLists.txt
+
+mkdir -p ./AppDir/bin
+cmake -S ./iris -B ./iris/build -DCMAKE_BUILD_TYPE=Release
+cmake --build ./iris/build -j$(nproc)
+mv -v ./iris/build/iris ./AppDir/bin
