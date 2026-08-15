d12fc013e0e7899afe228af180a17f821b7dea55
Link4Electronics <link4electronics@gmail.com>
2026-08-15 19:51:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q iris-emu | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q iris-emu | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook:wayland-is-broken.hook"
@@ -15,8 +15,6 @@ export DEPLOY_VULKAN=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/iris-emu
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
